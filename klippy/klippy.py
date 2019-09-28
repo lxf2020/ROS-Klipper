88c150206c529dd5c49a5ad5bb9fa0361dbe4203
LXF <lxf@LXFdeAir.local>
2019-09-28 17:41:15 +0800

    6

--- klippy/klippy.py
+++ klippy/klippy.py
@@ -129,6 +129,7 @@ class Printer:
         for section_config in config.get_prefix_sections(''):
             self.try_load_module(config, section_config.get_name())
             logging.info(section_config) 
+            logging.info(section_config.get_name()) 
         logging.info("fffffffffffffffffffffffffffffffffff") 
         for m in [toolhead]:
             m.add_printer_objects(config)
