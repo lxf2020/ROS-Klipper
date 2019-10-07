2a895b9d8d7df39e2d9f1746edb69efd0c89c5d6
XueFeng <40377958+lxf2020@users.noreply.github.com>
2019-10-07 15:25:07 +0800

    Create a.bash

---
+++ a.bash
@@ -0,0 +1,10 @@
+cd ~/
+sudo rm -rf klipper
+git clone https://github.com/lxf2020/klipper
+./klipper/scripts/install-octopi.sh
+cd ~/klipper/
+make menuconfig
+make
+sudo service klipper stop
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AK0707K7-if00-port0
+sudo service klipper start
