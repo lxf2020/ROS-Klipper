7b591fb663c956333fda66a96f4b46d09ec1b404
LXF <lxf@LXFdeAir.local>
2019-10-18 18:55:43 +0800

    2

--- klippy/serialhdl.py
+++ klippy/serialhdl.py
@@ -82,13 +82,14 @@ class SerialReader:
                 logging.warn("Unable to open port: %s", e)
                 self.reactor.pause(connect_time + 5.)
                 continue
+            logging.info("================this is serialhdl.connect============== ") 
             if self.baud:
                 stk500v2_leave(self.ser, self.reactor)
             self.serialqueue = self.ffi_lib.serialqueue_alloc(
                 self.ser.fileno(), 0)
             self.background_thread = threading.Thread(target=self._bg_thread)
             self.background_thread.start()
-            logging.info("================this is serialhdl.connect============== ") 
+            
             # Obtain and load the data dictionary from the firmware
             try:
                 identify_data = self._get_identify_data(connect_time + 5.)
