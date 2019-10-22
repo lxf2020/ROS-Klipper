ebb002d4f91676c70721916b0515e0d3efd12b15
LXF <lxf@LXFdeAir.local>
2019-10-22 18:06:46 +0800

    1

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -171,8 +171,7 @@ def get_ffi():
         for d in defs_all:
             FFI_main.cdef(d)
         FFI_lib = FFI_main.dlopen(os.path.join(srcdir, DEST_LIB))
-        logging.info("=========================FFI_lib =========================") 
-        logging.info("FFI_lib: "+FFI_lib) 
+        logging.info("FFI_lib: "+ FFI_lib) 
         logging.info("=========================get_ffi() END =========================")
         # Setup error logging
         def logging_callback(msg):
