b328505cb6dd3eb2071e03950d26a45ac659c7d1
LXF <lxf@LXFdeAir.local>
2019-10-25 15:47:48 +0800

    1

--- klippy/reactor.py
+++ klippy/reactor.py
@@ -263,10 +263,8 @@ class SelectReactor:
             g_next = ReactorGreenlet(run=self._dispatch_loop)
         g_next.parent = g.parent
         g.timer = self.register_timer(g.switch, waketime)
-        logging.info("12345678")
         self._next_timer = self.NOW
-        logging.info("====================== call "+g_next.switch().__name__+" ========================")
-        logging.info("123456789___")
+        logging.info("=========================== call switch():self._dispatch_loop =============================")
         # Switch to _dispatch_loop (via _end_greenlet or direct)
         eventtime = g_next.switch()
         logging.info("======================= reactor.pause()-[SelectReactor] END =======================")
