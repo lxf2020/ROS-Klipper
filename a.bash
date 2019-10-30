7694c107005c4d68770204262acef01fb7a09524
XueFeng <40377958+lxf2020@users.noreply.github.com>
2019-10-30 20:09:23 +0800

    Update a.bash

--- a.bash
+++ a.bash
@@ -8,6 +8,7 @@ cd ~/klipper/
 make menuconfig
 make
 sudo service klipper stop
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AK0707K7-if00-port0
+#make flash FLASH_DEVICE=/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AK0707K7-if00-port0
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A107ZEBN-if00-port0
 sudo service klipper start
 nano /tmp/klippy.log
